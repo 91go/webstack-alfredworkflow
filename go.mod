26dc8fa851a359503a0b44fd4206e97e253a6381
x1a0xf <yyzw@live.com>
2023-02-07 11:25:01 +0800

    feat: 通过判断html是否修改，判断是否读取缓存 #3 #1

--- go.mod
+++ go.mod
@@ -3,14 +3,16 @@ module wsaw
 go 1.19
 
 require (
+	github.com/PuerkitoBio/goquery v1.8.0
 	github.com/deanishe/awgo v0.29.1
-	gopkg.in/yaml.v3 v3.0.1
 )
 
 require (
+	github.com/andybalholm/cascadia v1.3.1 // indirect
 	github.com/magefile/mage v1.14.0 // indirect
 	github.com/stretchr/testify v1.8.1 // indirect
 	go.deanishe.net/env v0.5.1 // indirect
 	go.deanishe.net/fuzzy v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 	golang.org/x/text v0.6.0 // indirect
 )
--- main.go
+++ main.go
@@ -1,38 +1,29 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
 	"strings"
 	"time"
 
+	query "github.com/PuerkitoBio/goquery"
 	aw "github.com/deanishe/awgo"
-	"gopkg.in/yaml.v3"
 )
 
-type Webstack struct {
-	Title       string `yaml:"title"`
-	Description string `yaml:"description"`
-	Favicon     string `yaml:"favicon"`
-	URL         string `yaml:"url"`
-	Github      string `yaml:"github"`
-	Footer      string `yaml:"footer"`
-	Template    string `yaml:"template"`
-	Content     struct {
-		Categories `yaml:"categories"`
-	} `yaml:"content"`
+type Categories struct {
+	Name  string `json:"cateName"`
+	Sites []Site `json:"sites"`
 }
 
-type Categories []struct {
-	Name  string `yaml:"name"`
-	Sites `yaml:"sites"`
-}
-
-type Sites []struct {
-	Name        string `yaml:"name"`
-	Description string `yaml:"description,omitempty"`
-	URL         string `yaml:"url"`
-	Icon        string `yaml:"icon,omitempty"`
+type Site struct {
+	Name        string
+	Description string `json:"description,omitempty"`
+	URL         string
+	Icon        string
 }
 
 // Workflow is the main API
@@ -51,8 +42,6 @@ func main() {
 func run() {
 	var err error
 	args := wf.Args()
-	// log.Println("args: ", args, len(args))
-	// log.Println("os.Args: ", os.Args, len(os.Args))
 	if len(args) == 0 || len(args) > 2 {
 		return
 	}
@@ -64,19 +53,17 @@ func run() {
 	}()
 
 	fi := args[0]
-	// log.Println("fi: ", fi)
 
-	wsURL, b := wf.Alfred.Env.Lookup("ws_url")
+	url, b := wf.Alfred.Env.Lookup("url")
 	if !b {
 		return
 	}
 
-	// cate := getCategoriesFromConfig()
-	cate := getCategoriesFromCacheOrConfig(wsURL)
+	cate := getCategoriesFromCacheOrConfig(url)
 	allSites := extractAllSitesFromCategories(cate)
 	cateNames := extractNameFromCategories(cate)
 	names := matchFiAndCategoryNames(fi, cateNames)
-	var res Sites
+	var res []Site
 
 	if len(args) == 2 {
 		se := args[1]
@@ -95,58 +82,95 @@ func run() {
 	wf.SendFeedback()
 }
 
-// 用viper从config.yml中读取key为categories的数据
-// 读取到的数据是一个数组，数组中的每个元素是一个map
-// map中的key是name，value是sites
-// sites是一个数组，数组中的每个元素是一个map
-// map中的key是name，description，url，icon
-// func getCategoriesFromConfig() Categories {
-// 	viper.SetConfigName("config")
-// 	viper.SetConfigType("yml")
-// 	viper.AddConfigPath(".")
-// 	err := viper.ReadInConfig()
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	var webstack Webstack
-// 	err = viper.Unmarshal(&webstack)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return webstack.Content.Categories
-// }
-
 // 使用LoadOrStoreJSON直接从缓存中读取数据
-func getCategoriesFromCacheOrConfig(wsURL string) Categories {
-	var cate Categories
+func getCategoriesFromCacheOrConfig(url string) []Categories {
+	var older []Categories
 
-	err := wf.Cache.LoadOrStoreJSON("categories", 20*time.Minute, func() (interface{}, error) {
-		return getCategoriesFromConfigURL(wsURL), nil
-	}, &cate)
+	// 判断网页是否修改，如果未修改，则直接读取
+	if !determineContentIsModified(url) {
+		err := wf.Cache.LoadJSON("categories", &older)
+		if err != nil {
+			panic(err)
+		}
+		return older
+	}
+	// 如果网页修改，则重新获取
+	newer := getCategoriesFromConfigURL(url)
+	err := wf.Cache.StoreJSON("categories", newer)
 	if err != nil {
 		panic(err)
 	}
-	return cate
+	return newer
 }
 
 // 直接从url中获取categories
-func getCategoriesFromConfigURL(wsURL string) Categories {
-	resp, err := http.Get(wsURL)
+func getCategoriesFromConfigURL(url string) (cate []Categories) {
+	doc := FetchHTML(url)
+
+	doc.Find(".row").Each(func(i int, s *query.Selection) {
+		var sites []Site
+		s.Find(".col-sm-3").Each(func(i int, se *query.Selection) {
+			siteName := se.Find(".xe-comment a strong").Text()
+			siteURL, _ := se.Find(".label-info").Attr("data-original-title")
+			siteDes := se.Find(".xe-comment p").Text()
+			icon := se.Find(".xe-user-img img").AttrOr("data-src", "")
+			sites = append(sites, Site{
+				Name:        siteName,
+				URL:         siteURL,
+				Description: siteDes,
+				Icon:        icon,
+			})
+		})
+		name := s.Prev().Text()
+		cate = append(cate, Categories{
+			Name:  name,
+			Sites: sites,
+		})
+	})
+	return cate
+}
+
+// 判断网页是否修改，通过MD5值判断
+// true: 已修改
+// false: 未修改
+func determineContentIsModified(url string) bool {
+	var oldMD5 []byte
+	// 从缓存中读取旧的md5值（如果没有则重新获取）
+	err := wf.Cache.LoadOrStoreJSON("md5", 0*time.Minute, func() (interface{}, error) {
+		return getMD5FromURL(url), nil
+	}, &oldMD5)
+	if err != nil {
+		return false
+	}
+	// 新旧md5不同，说明网页修改，则重新获取
+	newMD5 := getMD5FromURL(url)
+	if !bytes.Equal(oldMD5, newMD5) {
+		err := wf.Cache.Store("md5", newMD5)
+		if err != nil {
+			panic(err)
+		}
+		return true
+	}
+
+	return false
+}
+
+// 从url中获取MD5值
+func getMD5FromURL(url string) []byte {
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
 	defer resp.Body.Close()
-
-	var webstack Webstack
-	err = yaml.NewDecoder(resp.Body).Decode(&webstack)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
 		panic(err)
 	}
-	return webstack.Content.Categories
+	return []byte(fmt.Sprintf("%x", md5.Sum(body)))
 }
 
 // 提取categories中的name
-func extractNameFromCategories(categories Categories) []string {
+func extractNameFromCategories(categories []Categories) []string {
 	var categoryNames []string
 	for _, v := range categories {
 		categoryNames = append(categoryNames, v.Name)
@@ -155,10 +179,9 @@ func extractNameFromCategories(categories Categories) []string {
 }
 
 // 提取categories中的所有sites
-func extractAllSitesFromCategories(categories Categories) []Sites {
-	var sites []Sites
+func extractAllSitesFromCategories(categories []Categories) (sites []Site) {
 	for _, v := range categories {
-		sites = append(sites, v.Sites)
+		sites = append(sites, v.Sites...)
 	}
 	return sites
 }
@@ -175,7 +198,7 @@ func matchFiAndCategoryNames(fi string, categoryNames []string) []string {
 }
 
 // 根据cate的名称，提取某个cate下的所有sites
-func extractSitesFromCategory(cate string, categories Categories) (sites Sites) {
+func extractSitesFromCategory(cate string, categories []Categories) (sites []Site) {
 	for _, v := range categories {
 		if v.Name == cate {
 			sites = v.Sites
@@ -186,12 +209,10 @@ func extractSitesFromCategory(cate string, categories Categories) (sites Sites) 
 
 // 如果匹配不到，则全局搜索所有的site
 // 将fi和Sites中的Site中的Name和Url进行匹配，如果能匹配到，组装为[]Sites，，最终将数据转为json格式，如果没有则为空json
-func matchFiAndSites(fi string, allSites []Sites) (sites Sites) {
-	for _, v := range allSites {
-		for _, s := range v {
-			if strings.Contains(strings.ToLower(s.Name), strings.ToLower(fi)) || strings.Contains(strings.ToLower(s.URL), strings.ToLower(fi)) {
-				sites = append(sites, s)
-			}
+func matchFiAndSites(fi string, allSites []Site) (sites []Site) {
+	for _, s := range allSites {
+		if strings.Contains(strings.ToLower(s.Name), strings.ToLower(fi)) || strings.Contains(strings.ToLower(s.URL), strings.ToLower(fi)) {
+			sites = append(sites, s)
 		}
 	}
 	return
@@ -199,8 +220,7 @@ func matchFiAndSites(fi string, allSites []Sites) (sites Sites) {
 
 // kw <directory-name> <url-name>... 首字母搜索，只在该分类下搜索
 // 先对fi与Categories的Name进行匹配，然后对其下的Sites的name和URL与se进行匹配
-func matchSeAndSites(fi, se string, categories Categories) Sites {
-	var sites Sites
+func matchSeAndSites(fi, se string, categories []Categories) (sites []Site) {
 	for _, category := range categories {
 		if category.Name == fi {
 			for _, s := range category.Sites {
@@ -215,34 +235,29 @@ func matchSeAndSites(fi, se string, categories Categories) Sites {
 }
 
 // 根据sitesFromCategory生成items，Site和Item相对应，其中Item的Arg为Site的URL，Title为Site的Name，Subtitle为Site的Description
-func generateItemsFromSites(Sites Sites) (items []aw.Item) {
-	for _, s := range Sites {
+func generateItemsFromSites(sites []Site) (items []aw.Item) {
+	for _, s := range sites {
 		wf.NewItem(s.Name).Arg(s.URL).Subtitle(s.Description).
-			Valid(true).Autocomplete(s.Name)
+			Valid(true).Autocomplete(s.Name).Icon(&aw.Icon{Value: s.Icon, Type: aw.IconTypeFileIcon})
 	}
 	return
 }
 
-// 从url中提取domain
-// func extractDomainFromURL(siteURL string) string {
-// 	u, err := url.Parse(siteURL)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	return u.Host
-// }
+func FetchHTML(url string) *query.Document {
+	resp, err := http.Get(url)
+	if err != nil {
+		return &query.Document{}
+	}
+	defer resp.Body.Close()
+	return DocQuery(resp)
+}
 
-// 从url中提取domain，判断该网站根目录是否有favicon.ico，如果有则返回favicon.ico的url，如果没有则返回customIcon，作为icon的path
-// icon的type为fileicon
-// func getIconFromURL(url, customIcon string) string {
-// 	domain := extractDomainFromURL(url)
-// 	iconURL := fmt.Sprintf("http://%s/favicon.ico", domain)
-// 	resp, err := http.Get(iconURL)
-// 	if err != nil {
-// 		panic(err)
-// 	}
-// 	if resp.StatusCode == 200 {
-// 		return iconURL
-// 	}
-// 	return customIcon
-// }
+// 请求goquery
+func DocQuery(resp *http.Response) *query.Document {
+	doc, err := query.NewDocumentFromReader(resp.Body)
+	if err != nil {
+		return &query.Document{}
+	}
+
+	return doc
+}
